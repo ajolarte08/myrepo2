c791cc9ee30cf8175330decff8510b6af67ba4a1
ajolarte08 <khalilangeloacosta@gmail.com>
2024-01-10 22:53:46 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,72 @@
+from flask import Flask, request, jsonify
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'new_schema1'
+
+mysql = MySQL(app)
+
+with app.app_context():
+    cur = mysql.connection.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS dogs (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            name VARCHAR(255),
+            gender VARCHAR(255),
+            date_of_birth DATE,
+            place_of_birth VARCHAR(255)
+        )
+    ''')
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS health_records (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            summary VARCHAR(255),
+            details VARCHAR(255),
+            dogs_id INT,
+            vets_id INT,
+            FOREIGN KEY (dogs_id) REFERENCES dogs(id)
+            FOREIGN KEY (vets_id) REFERENCES vets(id)
+        )
+    ''')
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS vets (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            name VARCHAR(255),
+        )
+    ''')
+    mysql.connection.commit()
+    cur.close()
+
+@app.route('/dogs', methods=['GET'])
+def get_dogs():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM dogs')
+    dogs = cur.fetchall()
+    cur.close()
+    return jsonify({'dogs': dogs})
+
+
+@app.route('/health_records', methods=['GET'])
+def get_health_records():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM health_records')
+    records = cur.fetchall()
+    cur.close()
+    return jsonify({'health_records': records})
+
+
+@app.route('/vets', methods=['GET'])
+def get_vets():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM vets')
+    vets = cur.fetchall()
+    cur.close()
+    return jsonify({'vets': vets})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
